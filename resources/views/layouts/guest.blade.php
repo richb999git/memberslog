0800ddc3b8ee43e74ad45f0d91e6add88a714502
Richard Braunton <richard@computech.software>
2021-10-17 19:07:33 +0100

    Add navigation menu to guest page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,6 +17,8 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
+        @include('layouts.navigation')
+
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>

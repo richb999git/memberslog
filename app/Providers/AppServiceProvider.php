42dddc71a6da8e4398946fbe318b1dec2eaf546c
Richard Braunton <richard@computech.software>
2022-03-09 15:46:43 +0000

    Correct function name to forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }

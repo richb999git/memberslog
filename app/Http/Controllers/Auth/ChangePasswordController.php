d4e30c7861089fd212228836107c53961f7e0cf7
Richard Braunton <richard@computech.software>
2022-03-09 12:33:27 +0000

    Add ability for admin to edit a member's password

---
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Input;
+
+class ChangePasswordController extends Controller
+{
+    /**
+     * Display the change password view.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
+     */
+    public function edit(Request $request)
+    {
+        return view('auth.change-password', ['request' => $request, 'member' => false, 'hidden' => 'hidden']);
+    }
+
+    /**
+     * Display the change password view for Admin to change any member.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
+     */
+    public function editMember(Request $request)
+    {
+        $user = Auth::user();
+
+        if (!$user || !$user->is_admin) return redirect()->route('home');
+
+        return view('auth.change-password', ['request' => $request, 'member' => true, 'hidden' => 'hidden']);
+    }
+
+    /**
+     * Handle a change of password request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::where('id', Auth::user()->id)->first();
+
+        $user->password = Hash::make($request->password);
+        $user->update();
+
+        return redirect()->route('home');
+    }
+
+    /**
+     * Handle a change of password request for Admin to update any user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function storeMember(Request $request)
+    {
+        $member = User::where('email', $request->email)->first();
+
+        if (!$member) {
+            return view('auth.change-password', [
+                'request' => $request,
+                'member' => true,
+                'hidden' => ''
+            ]);
+        }
+
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $member->password = Hash::make($request->password);
+        $member->update();
+
+        return redirect()->route('home');
+    }
+}
---
+++ resources/views/auth/change-password.blade.php
@@ -0,0 +1,62 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ isset($member) && $member === true ? route('changeMemberPassword') : route('changePassword') }}">
+            @csrf
+
+            <!-- Email Address if -->
+            @if (isset($member) && $member === true)
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            </div>
+            @endif
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                    type="password"
+                                    name="password_confirmation" required />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a href="{{url('membershipTypes')}}" class="btn btn-default mr-3 p-3 rounded-lg text-xs hover:bg-gray-100">CANCEL</a>
+                <x-button>
+                    {{ __('Change Password') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+
+    <div class="{{$hidden}}">
+        <div class="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
+            <div class="bg-white px-16 py-14 text-center rounded-md shadow-lg">
+                <h1 class="text-xl mb-4 font-bold text-slate-500">Email not found</h1>
+                <p class="text-md mb-4 text-slate-500">No member with that email.</p>
+                <a
+                    href="{{ url('changeMemberPassword') }}"
+                    class="ml-0 mb-1 p-2 pr-4 pl-4 rounded-sm ring-2 bg-indigo-200 text-black-600 hover:bg-indigo-400 hover:text-white">
+                    OK
+                </a>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
